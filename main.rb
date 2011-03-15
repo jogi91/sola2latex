74449e4c894c647271e4769db3d4cc6b31e9e5f4
Johannes Weinbuch <jweinbuch@gmx.ch>
2011-03-15 11:09:19 +0100

    Weiterarbeit waehrend EInf
    * Erste Stylings
    * Programm startet erst nach Warnhinweis

--- main.rb
+++ main.rb
@@ -1,13 +1,21 @@
-Shoes.app do
-  # Werte für die Blockdaten abfragen
-  parameter = ["Blockname", "J+S-Nummer", "Leitung", "Teilnehmer", "Ort", "Datum", "Zeit"]
-  parameterwerte = Array.new(parameter.size){String.new}
-  debug parameterwerte.inspect
-  
-  parameter.each_index { |index|
-     parameterwerte[index] = ask("#{parameter[index]}?")
-  }
-  
-  debug parameterwerte.inspect
+Shoes.app(:title => "Solablock2LaTeX", :width => 800, :height => 800, :resizable => true) do
+  #Warnhinweis 
+  @hinweis = stack(:width => 790, :height => 790) do
+    background red
+    @knopf = button("ok"){
+      #Wenn der Knopf gedrückt wird, erste Werte einlesen
+      # Werte für die Blockdaten abfragen
+       parameter = ["Blockname", "J+S-Nummer", "Leitung", "Teilnehmer", "Ort", "Datum", "Zeit"]
+       parameterwerte = Array.new(parameter.size){String.new}
+       debug parameterwerte.inspect
+
+       parameter.each_index { |index|
+          parameterwerte[index] = ask("#{parameter[index]}?")
+          para parameterwerte[index]
+       }
+    } 
+    @knopf.style(:width => 100, :center => true)
+  end
+ 
     
 end
