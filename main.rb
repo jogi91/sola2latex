0271f6ddba2eea2b58b9c835465eb26ff6413e2d
Johannes Weinbuch <jweinbuch@gmx.ch>
2011-03-16 21:51:39 +0100

    erste Eingabefunktion fertig
    * Manipulation von Stack funktioniert jetzt so, wie es soll,mit der append-Methode

--- main.rb
+++ main.rb
@@ -8,13 +8,20 @@ Shoes.app(:title => "Solablock2LaTeX", :width => 800, :height => 800, :resizable
        parameter = ["Blockname", "J+S-Nummer", "Leitung", "Teilnehmer", "Ort", "Datum", "Zeit"]
        parameterwerte = Array.new(parameter.size){String.new}
        debug parameterwerte.inspect
-
-       parameter.each_index { |index|
-          parameterwerte[index] = ask("#{parameter[index]}?")
-          para parameterwerte[index]
+       parameter.each_index{|index|
+       	debug index.inspect
+       	parameterwerte[index] = ask("#{parameter[index]}?")
+       	@hinweis.append do #Anderenfalls würden die paragraphs ausserhalb von @hinweis platziert werden.
+       	  para "#{parameter[index]}: #{parameterwerte[index]}"
+     	  end
        }
-    } 
+       debug @daten.inspect
+     }
+     title "Blockdaten:"
+    
+     
     @knopf.style(:width => 100, :center => true)
+    para "test"
   end
  
     
