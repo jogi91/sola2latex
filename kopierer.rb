4e8854c08d1c1905afb764152ce514fe17da064a
Johannes Weinbuch <jweinbuch@gmx.ch>
2011-03-28 21:22:57 +0200

    Klasse Kopierer hinzugefuegt, die die Vorlage verwaltet.

---
+++ kopierer.rb
@@ -0,0 +1,8 @@
+#Diese Klasse soll den Vorlagentext komplett enthalten, da ich nicht davon ausgehen kann, dass der LaTeX-Code von Shoes mitgepackt wird.
+#Er wird die Daten entgegennehmen, sie komplett in den Block einfügen und den kompletten LaTeX-Code dann ausgeben können.
+class Kopierer
+  def initialize
+    
+  end  
+  
+end
