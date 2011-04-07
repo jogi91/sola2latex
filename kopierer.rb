b391e58c613f0c8c58276215a75c22240999a552
Johannes Weinbuch <jweinbuch@gmx.ch>
2011-04-07 17:07:31 +0200

    Instanzvariabeln, die Kommentare beinhalten hinzugefuegt, Texte werden als leerstrings initialisiert

--- kopierer.rb
+++ kopierer.rb
@@ -2,9 +2,9 @@
 #Er wird die Daten entgegennehmen, sie komplett in den Block einfügen und den kompletten LaTeX-Code dann ausgeben können.
 class Kopierer
   attr_accessor :basisparameter, :basisparameterwerte, :ziele, :sicherheit
-  attr_accessor :einleitung, :hauptteil, :ausstieg
+  attr_accessor :einstieg, :hauptteil, :ausstieg, :kommentare_einstieg, :kommentare_hauptteil, :kommentare_ausstieg
   def initialize
-    
+    @einstieg, @hauptteil, @ausstieg, @kommentare_einleitung, @kommentare_hauptteil, @kommentare_ausstieg = ""
   end  
   
 end
